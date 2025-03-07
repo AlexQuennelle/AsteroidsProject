5e24841e104886dbc897c2dffaf72bb4203f97ba
Alex Quennelle <bluephoenixcg@gmail.com>
2025-03-07 11:55:14 -0600

    Prettify things

--- sketch.js
+++ sketch.js
@@ -13,8 +13,21 @@ function draw() {
   push();
   noStroke();
   fill(50);
-  rect(0,0,(width - gameInstance.resolution.x) / 2, height);
-  rect((width / 2 + gameInstance.resolution.x / 2),0,(width - gameInstance.resolution.x) / 2, height);
+  rect(0, 0, (width - gameInstance.resolution.x) / 2, height);
+  rect(
+    width / 2 + gameInstance.resolution.x / 2,
+    0,
+    (width - gameInstance.resolution.x) / 2,
+    height,
+  );
+
+  rect(0, 0, width, (height - gameInstance.resolution.y) / 2);
+  rect(
+    0,
+    height / 2 + gameInstance.resolution.y / 2,
+    width,
+    (height - gameInstance.resolution.y) / 2,
+  );
   pop();
 }
 
